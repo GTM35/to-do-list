30fa2e952709dba36759294b49852d045177322e
GTM35 <035guuh@gmail.com>
2025-01-27 09:06:47 -0300

    fix: correção da imagem em produção.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    assetsInlineLimit: 0, // Evita que pequenas imagens sejam embutidas como base64
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name][extname]", // Nome dos arquivos na build
+      },
+    },
+  },
+});
